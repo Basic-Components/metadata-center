1b6873a22c02f1afb8131ac9c06ffba6f4356196
hsz1273327 <hsz1273327@gmail.com>
2020-06-07 08:43:28 +0800

    update

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 $env:IMG_VERSION="dev-0.0.1"
-docker build -t 47.98.42.103:8333/service/business_config_center:$env:IMG_VERSION .
-docker push 47.98.42.103:8333/service/business_config_center:$env:IMG_VERSION
+docker build -t hsz1273327/components_manager:$env:IMG_VERSION .
+docker push hsz1273327/components_manager:$env:IMG_VERSION
